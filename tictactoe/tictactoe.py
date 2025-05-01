4bc066b7e0f8fde25aceebfe5e541e3118103020
Tony Fiore <tdfiore@gmail.com>
2025-05-01 13:25:18 -0700

    Implement tictactoe actions

--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -38,7 +38,13 @@ def actions(board):
     """
     Returns set of all possible actions (i, j) available on the board.
     """
-    raise NotImplementedError
+    result = set()
+    for i in range(len(board)):
+        for j in range(len(board[i])):
+            if board[i][j] == EMPTY:
+                result += (i, j)
+
+    return result
 
 
 def result(board, action):
