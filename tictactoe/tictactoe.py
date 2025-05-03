f1ddb14b7076c0e8ccea5ec8902f3d64d05eb9a7
Tony Fiore <tdfiore@gmail.com>
2025-05-02 18:30:35 -0700

    Fix winner

--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -65,15 +65,15 @@ def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
-    if board[0][0] == board[0][1] == board[0][2] or board[0][0] == board[1][0] == board[2][0] or board[0][0] == board[1][1] == board[2][2]:
+    if board[0][0] and (board[0][0] == board[0][1] == board[0][2] or board[0][0] == board[1][0] == board[2][0] or board[0][0] == board[1][1] == board[2][2]):
         return board[0][0]
-    elif board[0][1] == board[1][1] == board [2][1]:
+    elif board[0][1] and (board[0][1] == board[1][1] == board [2][1]):
         return board[0][1]
-    elif board[0][2] == board[1][2] == board[2][2] or board[0][2] == board[1][1] == board[2][0]:
+    elif board[0][2] and (board[0][2] == board[1][2] == board[2][2] or board[0][2] == board[1][1] == board[2][0]):
         return board[0][2]
-    elif board[1][0] == board[1][1] == board[1][2]:
+    elif board[1][0] and (board[1][0] == board[1][1] == board[1][2]):
         return board[1][0]
-    elif board[2][0] == board[2][1] == board[2][2]:
+    elif board[2][0] and (board[2][0] == board[2][1] == board[2][2]):
         return board[2][0]
 
     return None
