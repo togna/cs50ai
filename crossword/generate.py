5ed7ecab971b1841697387929bc30ffa37fb5d8f
Tony Fiore <tdfiore@gmail.com>
2025-06-13 17:23:52 -0700

    Implement assignment_complete

--- crossword/generate.py
+++ crossword/generate.py
@@ -165,7 +165,11 @@ class CrosswordCreator():
         Return True if `assignment` is complete (i.e., assigns a value to each
         crossword variable); return False otherwise.
         """
-        raise NotImplementedError
+        for var in self.crossword.variables:
+            if assignment.get(var) is None:
+                return False
+
+        return True
 
     def consistent(self, assignment):
         """
