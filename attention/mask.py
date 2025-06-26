ff4636478f65055b11abd6f64461b5a2c8e2b00b
Tony Fiore <tdfiore@gmail.com>
2025-06-26 15:15:15 -0700

    Implement get_mask_token_index

--- attention/mask.py
+++ attention/mask.py
@@ -45,8 +45,11 @@ def get_mask_token_index(mask_token_id, inputs):
     Return the index of the token with the specified `mask_token_id`, or
     `None` if not present in the `inputs`.
     """
-    # TODO: Implement this function
-    raise NotImplementedError
+    for index, id in enumerate(inputs.input_ids[0]):
+        if id == mask_token_id:
+            return index
+
+    return None
 
 
 
